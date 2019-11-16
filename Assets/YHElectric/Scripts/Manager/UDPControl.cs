915329b325e54f0bc1ddf532a30b0b2721581aa0
xibinbin <1290212854@qq.com>
2019-11-16 09:23:12 +0800

    init commit

---
+++ Assets/YHElectric/Scripts/Manager/UDPControl.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class UDPControl : MonoBehaviour
+{
+    
+    enum MouseEventFlag : uint
+    {
+        Move = 0x0001,
+        LeftDown = 0x0002,
+        LeftUp = 0x0004,
+        RightDown = 0x0008,
+        RightUp = 0x0010,
+        MiddleDown = 0x0020,
+        MiddleUp = 0x0040,
+        XDown = 0x0080,
+        XUp = 0x0100,
+        Wheel = 0x0800,
+        VirtualDesk = 0x4000,
+        Absolute = 0x8000
+    }
+
+    public static UDPControl instance;
+    private int port=10001;
+    public LGZN.Net.UDPClient uDPClient;
+
+    
+    private void Awake()
+    {
+        instance = this;
+        uDPClient = new LGZN.Net.UDPClient(port, LGZN.Net.UDPMode.SendAndReceive);
+    }
+    private void Start()
+    {
+        uDPClient.Receive(OnReceive);
+    }
+    private void OnReceive(string str)
+    {
+        print(str);
+    }
+    int mousex, mousey;
+    bool isClick = false;
+    private void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    GetCursorPos(ref mousex, ref mousey);
+        //    mouse_event(MouseEventFlag.LeftDown, 0, 0, 0, UIntPtr.Zero);
+        //    mouse_event(MouseEventFlag.LeftUp, 0, 0, 0, UIntPtr.Zero);
+        //}
+       
+    }
+    [DllImport("user32")]
+    public static extern int SetCursorPos(int x, int y);
+    [DllImport("user32")]
+    private static extern bool GetCursorPos(ref int x, ref int y);
+    [DllImport("user32")]
+    static extern void mouse_event(MouseEventFlag flags, int dx, int dy, uint data, UIntPtr extraInfo);
+
+    private void OnDestroy()
+    {
+        uDPClient.Close();
+    }
+    private void OnApplicationQuit()
+    {
+        uDPClient.Close();
+    }
+}
---
+++ Assets/YHElectric/Scripts/Manager/UIManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+    private float timer = 0;
+    void Awake()
+    {
+        instance = this;
+    }
+    private void Start()
+    {
+        transform.Find("bg/topBar/water").GetComponent<Button>().onClick.AddListener(() => BtnClick("water"));
+        transform.Find("bg/monitoring/transformer").GetComponent<Button>().onClick.AddListener(() => BtnClick("transformer"));
+        transform.Find("bg/function/energyCloud").GetComponent<Button>().onClick.AddListener(() => BtnClick("energyCloud"));
+        transform.Find("bg/cameraMove/cameraMoveUp").GetComponent<Button>().onClick.AddListener(() => BtnClick("cameraMoveUp"));
+        transform.Find("bg/cameraMove/cameraMoveForward").GetComponent<Button>().onClick.AddListener(() => BtnClick("cameraMoveForward"));
+        transform.Find("bg/cameraMove/cameraMoveDown").GetComponent<Button>().onClick.AddListener(() => BtnClick("cameraMoveDown"));
+        transform.Find("bg/cameraMove/cameraMoveBack").GetComponent<Button>().onClick.AddListener(() => BtnClick("cameraMoveBack"));
+        //transform.Find("bg/cameraRot/cameraRotUp").GetComponent<Button>().onClick.AddListener(() => BtnClick("cameraRotUp"));
+        //transform.Find("bg/cameraRot/cameraRotDown").GetComponent<Button>().onClick.AddListener(() => BtnClick("cameraRotDown"));
+        //transform.Find("bg/cameraRot/cameraRotLeft").GetComponent<Button>().onClick.AddListener(() => BtnClick("cameraRotLeft"));
+        //transform.Find("bg/cameraRot/cameraRotRight").GetComponent<Button>().onClick.AddListener(() => BtnClick("cameraRotRight&123.3"));
+        transform.Find("bg/closeBtn").GetComponent<Button>().onClick.AddListener(() => BtnClick("closeBtn"));
+    }
+    private void Update()
+    {
+        timer += Time.deltaTime;
+    }
+    private void BtnClick(string str)
+    {
+        print(str);
+        UDPControl.instance.uDPClient.Send(str);
+    }
+    public void OnCamRot(Vector2 vector2)
+    {
+        if (timer < 0.2f) return;
+        UDPControl.instance.uDPClient.Send("OnCamRotX&" + vector2.x);
+        UDPControl.instance.uDPClient.Send("OnCamRotY&" + vector2.y);
+        timer = 0;
+    }
+    public void OnCamRotEnd()
+    {
+        UDPControl.instance.uDPClient.Send("OnCamRotX&" + 0);
+        UDPControl.instance.uDPClient.Send("OnCamRotY&" + 0);
+    }
+}
