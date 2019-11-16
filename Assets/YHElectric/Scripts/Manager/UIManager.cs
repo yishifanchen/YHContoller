87387dc526624e4601839f87da06b00894a76637
xibinbin <1290212854@qq.com>
2019-11-16 14:58:15 +0800

    略

--- Assets/YHElectric/Scripts/Manager/UIManager.cs
+++ Assets/YHElectric/Scripts/Manager/UIManager.cs
@@ -13,19 +13,27 @@ public class UIManager : MonoBehaviour
     }
     private void Start()
     {
+        //天气
         for (int i = 0; i < transform.Find("bg/weather").childCount; i++)
         {
             int index = i;
             transform.Find("bg/weather").GetChild(i).GetComponent<Button>().onClick.AddListener(() => BtnClick("SetWeather&"+index.ToString()));
         }
+        //时间
         transform.Find("bg/timeSlider").GetComponent<Slider>().onValueChanged.AddListener(BtnClick);
+        //顶部展板
         transform.Find("bg/topBar/water").GetComponent<Button>().onClick.AddListener(() => BtnClick("water"));
+        //实时监测
         transform.Find("bg/monitoring/transformer").GetComponent<Button>().onClick.AddListener(() => BtnClick("transformer"));
+        //平台功能
         transform.Find("bg/function/energyCloud").GetComponent<Button>().onClick.AddListener(() => BtnClick("energyCloud"));
+        //镜头操控
+        transform.Find("bg/roam").GetComponent<Button>().onClick.AddListener(() => BtnClick("roam"));
         transform.Find("bg/cameraMove/cameraMoveUp").GetComponent<Button>().onClick.AddListener(() => BtnClick("cameraMoveUp"));
         transform.Find("bg/cameraMove/cameraMoveForward").GetComponent<Button>().onClick.AddListener(() => BtnClick("cameraMoveForward"));
         transform.Find("bg/cameraMove/cameraMoveDown").GetComponent<Button>().onClick.AddListener(() => BtnClick("cameraMoveDown"));
         transform.Find("bg/cameraMove/cameraMoveBack").GetComponent<Button>().onClick.AddListener(() => BtnClick("cameraMoveBack"));
+        //关闭
         transform.Find("bg/closeBtn").GetComponent<Button>().onClick.AddListener(() => BtnClick("closeBtn"));
     }
     private void Update()
