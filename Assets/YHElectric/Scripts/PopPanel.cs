1d8476c615bf4dc5e6b964c06fa4f7d0b59e73b7
xibinbin <1290212854@qq.com>
2019-11-26 14:52:33 +0800

    修改配置文件 添加点击按钮弹出子菜单功能

---
+++ Assets/YHElectric/Scripts/PopPanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopPanel : MonoBehaviour
+{
+    public static PopPanel instance;
+    public Transform popPanelBtnParent;
+    private void Awake()
+    {
+        instance = this;
+        Close();
+    }
+    void Start()
+    {
+        transform.Find("closeBtn").GetComponent<Button>().onClick.AddListener(Close);
+    }
+    void Close()
+    {
+        for(int i=0;i< popPanelBtnParent.childCount; i++)
+        {
+            Destroy(popPanelBtnParent.GetChild(i).gameObject);
+        }
+        transform.gameObject.SetActive(false);
+    }
+}
